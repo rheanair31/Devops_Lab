50e040739ae3d396c924beae9ae4a9b8e009d858
Rhea Nair <rhea31@somaiya.edu>
2025-11-06 01:03:54 +0530

    triggering workflow

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ app.get("/", (req, res) => {
   res.send("CI/CD pipeline using GitHub Actions is running successfully 🚀");
 });
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
